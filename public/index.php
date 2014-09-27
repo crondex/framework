1b1c8490425742bc62a76718c0fc249c10eb710f
Andrew McLaughlin <andrew@mclau.ghlin.com>
2014-09-27 00:07:54 -0700

    updated files to accomodate moving crondex to vendor/crondex/crondex

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ define('ROOT', dirname(dirname(__FILE__)) . DS);
 
 require_once (ROOT . 'app' . DS . 'config' . DS . 'env.php');
 require_once (ROOT . 'vendor' . DS . 'autoload.php');
-require_once (ROOT . 'vendor' . DS . 'crondex' . DS . src . DS . Bootstrap . DS . 'Bootstrap.php');
+require_once (ROOT . 'vendor' . DS . 'crondex' . DS . 'crondex' . DS . src . DS . Bootstrap . DS . 'Bootstrap.php');
 
 $configFilePath = ROOT . 'app' . DS . 'config' . DS . 'main.ini';
 $routesFilePath = ROOT . 'app' . DS . 'config' . DS . 'routes.ini';
